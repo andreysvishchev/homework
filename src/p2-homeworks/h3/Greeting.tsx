1056d6c2693a3c660831c5b1db5f5c1f2df93bd6
Andrey Svishchev <andrey.svishchev@yandex.ru>
2022-02-16 16:58:33 +0300

    hw3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+  name: string // need to fix any
+  setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+  addUser: () => void // need to fix any
+  error: string // need to fix any
+  totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+  { name, setNameCallback, addUser, error, totalUsers } // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
 
-    return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
-        </div>
-    )
+  const inputClass = error ? s.error : '' // need to fix with (?:)
+
+
+
+
+  return (
+    <div className={s.wrap}>
+      <div className={s.inner}>
+        <input value={name} onChange={setNameCallback} className={`${inputClass} ${s.input}`} placeholder='Введите имя' />
+        <button onClick={addUser} className={s.button} >add</button>
+        <span className={s.users}>{totalUsers}</span>
+      </div>
+
+      <span className={`${s.err} ${inputClass}`}>{error}</span>
+    </div>
+  )
 }
 
 export default Greeting
