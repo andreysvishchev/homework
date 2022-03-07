5e6efe6882a94e9a68b11cf7b80d04bd41ea0fcf
Andrey Svishchev <andrey.svishchev@yandex.ru>
2022-03-07 14:08:51 +0300

    Updates

---
+++ precache-manifest.eb19bc3b6fc82b7e954cab2560bc61c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af84100ef3860df8481f7e53c38eeff6",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "51f5b1613f1257f860e2",
+    "url": "/homework/static/css/main.7d294a84.chunk.css"
+  },
+  {
+    "revision": "2b4f2029f39d27f6da62",
+    "url": "/homework/static/js/2.cdc89d3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework/static/js/2.cdc89d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f5b1613f1257f860e2",
+    "url": "/homework/static/js/main.4b7074cb.chunk.js"
+  },
+  {
+    "revision": "8217a5d2c8eff62f32e7",
+    "url": "/homework/static/js/runtime-main.c49b04a8.js"
+  }
+]);
