61370ed6e209c85d3891b074e9cb852d6d0c61f1
Andrey Svishchev <andrey.svishchev@yandex.ru>
2022-04-19 22:42:40 +0300

    hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,35 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            if (action.payload === 'up') {
+                return [...state.sort((a, b) => a.name < b.name ? -1 : 1)]
+            } else if (action.payload === 'down') {
+                return [...state.sort((a, b) => a.name > b.name ? -1 : 1)]
+            } else {
+                return state
+            }
         }
         case 'check': {
             // need to fix
-            return state
+            return [...state.filter(el => el.age > action.payload)]
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+type ActionType = checkAction | sortAction
+
+type checkAction = {
+    type: 'check'
+    payload: 18
+}
+type sortAction = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+
